6c51f8aed2dd9a976f3f1ecfc867439a4fb52e4b
RigorityJ <rigorityj@gmail.com>
2018-09-06 12:47:33 -0400

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 java -jar main/build/libs/main.jar "jar" "testable-jar/build/libs/testable-jar.jar" "testable-jar/build/libs/testable-jar.jar"
-#java -jar main/build/libs/main.jar "apk" "/home/sazzad14/projects/program-analysis-soot/apk/crypto_test.apk"
+#java -jar main/build/libs/main.jar "apk" "/path/to/apk/crypto_test.apk"
 
