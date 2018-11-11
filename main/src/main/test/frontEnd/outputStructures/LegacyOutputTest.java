30b9e235e8985f102ab6bb409150fc599f7b58cb
franceme <frantzme@vt.edu>
2018-11-11 16:13:11 -0500

    Added Notes to the Legacy Output Alterations
    Signed-off-by: franceme <frantzme@vt.edu>

--- main/src/main/test/frontEnd/outputStructures/LegacyOutputTest.java
+++ main/src/main/test/frontEnd/outputStructures/LegacyOutputTest.java
@@ -35,79 +35,16 @@ public class LegacyOutputTest
 		StringBuilder sampleOne = new StringBuilder();
 
 		//region Creating Sample Output
-		//region OLD
-		/*
-		sampleOne.append("Analyzing "+this.type+": "+this.source);
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 5: Used export grade public Key\n");
-		sampleOne.append("***Cause: Used default key size in method: <tester.Crypto: java.security.KeyPair generateKeyPairDefaultKeySize()>[122]\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 5: Used export grade public Key\n");
-		sampleOne.append("***Found: [1024] in Method: <tester.Crypto: java.security.KeyPair generateKeyPair()>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 1: Found broken crypto schemes\n");
-		sampleOne.append("***Found: [\"AES/ECB/PKCS5.appendING\"] in Method: <tester.Crypto: void <init>()>\n");
-		sampleOne.append("***Found: [\"PBEWithMD5AndDES\"] in Method: <tester.PasswordUtils: void <init>(java.lang.String)>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=============================================\n");
-		sampleOne.append("***Violated Rule 13: Untrused PRNG (java.util.Random) Found in <tester.Crypto: byte[] randomNumberGeneration(long)>\n");
-		sampleOne.append("=============================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should at least get One accepted Issuer from Other Sources in getAcceptedIssuers method of tester.Crypto$2\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should not use unpinned self-signed certification in tester.Crypto$2\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 2: Found broken hash functions\n");
-		sampleOne.append("***Found: [\"MD5\"] in Method: <tester.PBEUsage: javax.crypto.spec.PBEKeySpec getPBEParameterSpec(java.lang.String)>\n");
-		sampleOne.append("***Found: [\"SHA\"] in Method: <tester.Crypto: void main(java.lang.String[])>\n");
-		sampleOne.append("***Found: [\"SHA1\"] in Method: <tester.Crypto: void main(java.lang.String[])>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 3: Used constant keys in code\n");
-		sampleOne.append("***Found: [\"aaaaaaa\"] in Method: <tester.LiveVarsClass: void <clinit>()>\n");
-		sampleOne.append("***Found: [\"tzL1AKl5uc4NKYaoQ4P3WLGIBFPXWPWdu1fRm9004jtQiV\"] in Line 79 in Method: <tester.PasswordUtils: void <init>(java.lang.String)>\n");
-		sampleOne.append("***Found: [\"aaaaaaa\"] in Line 4 in Method: <tester.LiveVarsClass: void <clinit>()>\n");
-		sampleOne.append("***Found: [\"helloworld\"] in Method: <tester.VeryBusyClass: void main(java.lang.String[])>\n");
-		sampleOne.append("***Found: [\"Bar12345Bar12345\"] in Line 152 in Method: <tester.Crypto: void main(java.lang.String[])>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 10: Found constant IV in code\n");
-		sampleOne.append("***Found: [\"aaaaaaa\"] in Method: <tester.LiveVarsClass: void <clinit>()>\n");
-		sampleOne.append("***Found: [\"aaaaaaa\"] in Line 4 in Method: <tester.LiveVarsClass: void <clinit>()>\n");
-		sampleOne.append("***Found: [\"RandomInitVector\"] in Line 153 in Method: <tester.Crypto: void main(java.lang.String[])>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 9: Found constant salts in code\n");
-		sampleOne.append("***Found: [\"f77aLYLo\"] in Line 80 in Method: <tester.PasswordUtils: void <init>(java.lang.String)>\n");
-		sampleOne.append("***Found: [\"helloworld\"] in Method: <tester.VeryBusyClass: void main(java.lang.String[])>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 8: Used < 1000 iteration for PBE\n");
-		sampleOne.append("***Found: [1] in Line 50 in Method: <tester.PBEUsage: javax.crypto.spec.PBEKeySpec getPBEParameterSpec(java.lang.String)>\n");
-		sampleOne.append("***Found: [17] in Method: <tester.PasswordUtils: void <init>(java.lang.String)>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 14: Used Predictable KeyStore Password\n");
-		sampleOne.append("***Found: [\"mypass\"] in Line 108 in Method: <tester.Crypto: java.security.KeyPair generateKeyPair()>\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 7: Used HTTP Protocol\n");
-		sampleOne.append("***Found: [\"http://publicobject.com/helloworld.txt\"] in Method: <tester.UrlFrameWorks: java.net.HttpURLConnection createURL(java.lang.String)>\n");
-		sampleOne.append("=======================================\n");
-		*/
-		//endregion
 		sampleOne.append("Analyzing " + this.type + ": " + this.source + "\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("***Violated Rule 5: Used export grade public Key\n");
 		sampleOne.append("***Cause: Used default key size in method: <tester.Crypto: java.security.KeyPair generateKeyPairDefaultKeySize()>[122]\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 5: Used export grade public Key\n");
-		sampleOne.append("***Found: [1024] in Method: <tester.Crypto: java.security.KeyPair generateKeyPair()>\n");
+		//Legacy Output Slight Alteration
+		//sampleOne.append("=======================================\n");
+		//sampleOne.append("=======================================\n");
+		//sampleOne.append("***Violated Rule 5: Used export grade public Key\n");
+		//sampleOne.append("***Found: ["1024"] in Method: <tester.Crypto: java.security.KeyPair generateKeyPair()>\n");
+		sampleOne.append("***Found: [\"1024\"] in Method: <tester.Crypto: java.security.KeyPair generateKeyPair()>\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("***Violated Rule 1: Found broken crypto schemes\n");
@@ -115,10 +52,14 @@ public class LegacyOutputTest
 		sampleOne.append("***Found: [\"PBEWithMD5AndDES\"] in Method: <tester.PasswordUtils: void <init>(java.lang.String)>\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should at least get One accepted Issuer from Other Sources in getAcceptedIssuers method of tester.Crypto$2\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("=======================================\n");
-		sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should not use unpinned self-signed certification in tester.Crypto$2\n");
+		//Legacy Output Slight Alteration
+		//sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should at least get One accepted Issuer from Other Sources in getAcceptedIssuers method of tester.Crypto$2\n");
+		//sampleOne.append("=======================================\n");
+		//sampleOne.append("=======================================\n");
+		//sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager ***Should not use unpinned self-signed certification in tester.Crypto$2\n");
+		sampleOne.append("***Violated Rule 4: Uses untrusted TrustManager\n");
+		sampleOne.append("***Should at least get One accepted Issuer from Other Sources in getAcceptedIssuers method of tester.Crypto$2\n");
+		sampleOne.append("***Should not use unpinned self-signed certification in tester.Crypto$2\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("=======================================\n");
 		sampleOne.append("***Violated Rule 2: Found broken hash functions\n");
