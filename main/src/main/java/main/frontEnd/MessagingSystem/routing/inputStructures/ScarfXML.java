65a828bf2eb3b792e36a28cb825d8c85d284f3bf
Maister <maister@desktop.com>
2018-12-14 15:46:40 -0500

    Upped to 01.01.07
    Created a ScarfXML input structure.
    
    Signed-off-by: Maister <maister@desktop.com>

---
+++ main/src/main/java/main/frontEnd/MessagingSystem/routing/inputStructures/ScarfXML.java
@@ -0,0 +1,174 @@
+package main.frontEnd.MessagingSystem.routing.inputStructures;
+
+import main.frontEnd.MessagingSystem.routing.EnvironmentInformation;
+import main.frontEnd.MessagingSystem.routing.Listing;
+import main.rule.engine.EngineType;
+
+import java.util.Arrays;
+
+/**
+ * @author RigorityJTeam
+ * Created on 2018-12-14.
+ * @since 01.01.07
+ *
+ * <p>The Scarf Input check implementation.</p>
+ */
+public class ScarfXML implements InputStructure {
+
+    private ScarfJsonInput inputReader;
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <p>
+     * The overridden method for the ScarfXML output.
+     */
+    public EnvironmentInformation inputValidation(String[] args, String dependencies, EngineType type) {
+
+        EnvironmentInformation info = new EnvironmentInformation(args[0], type, dependencies, Listing.ScarfXML.getFlag());
+
+        inputReader = new ScarfJsonInput(Arrays.copyOfRange(args, 0, args.length - 1));
+        info.setAssessmentFramework(inputReader.getAssessmentFramework());
+        info.setAssessmentFrameworkVersion(inputReader.getAssessmentFrameworkVersion());
+        info.setBuildRootDir(inputReader.getBuildRootDir());
+        info.setPackageRootDir(inputReader.getPackageRootDir());
+        info.setParserName(inputReader.getParserName());
+        info.setParserVersion(inputReader.getParserVersion());
+
+        return info;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <p>
+     * The overridden method for the Legacy output.
+     */
+    public String helpInfo() {
+        return inputReader.helpInfo();
+    }
+
+
+    //region Sub Class Reader
+
+    /**
+     * A sub class to read the information Scarf Input Reader
+     * This is externalized to be able to read from multiple forms of input
+     */
+    private class ScarfJsonInput {
+        //region Attributes
+        private String assessmentFramework = "UNAVAILABLE";
+        private String assessmentFrameworkVersion = "UNAVAILABLE";
+        private String buildRootDir = "UNAVAILABLE";
+        private String packageRootDir = "UNAVAILABLE";
+        private String parserName = "UNAVAILABLE";
+        private String parserVersion = "UNAVAILABLE";
+        //endregion
+
+
+        /**
+         * The constructor with raw string arguments
+         * Sets the parameters if any strings are set
+         *
+         * @param args {@link String[]} - The raw command arguments passed in from the command line.
+         */
+        public ScarfJsonInput(String[] args) {
+            if (args.length >= 1)
+                this.assessmentFramework = args[0];
+            if (args.length >= 2)
+                this.assessmentFramework = args[1];
+            if (args.length >= 3)
+                this.assessmentFramework = args[2];
+            if (args.length >= 4)
+                this.assessmentFramework = args[3];
+            if (args.length >= 5)
+                this.assessmentFramework = args[4];
+            if (args.length >= 6)
+                this.assessmentFramework = args[5];
+        }
+
+        public String helpInfo() {
+            StringBuilder help = new StringBuilder();
+
+            help.append("Usage: (AssessmentFramework) (AssessmentFrameworkVersion) (BuildRootDir) (PackageRootDir) (ParserName) (ParserVersion)\n");
+            help.append("\tAssessmentFramework: Default => STUBBED").append("\n");
+            help.append("\tAssessmentFrameworkVersion: Default => STUBBED").append("\n");
+            help.append("\tBuildRootDir: Default => STUBBED").append("\n");
+            help.append("\tPackageRootDir: Default => STUBBED").append("\n");
+            help.append("\tParserName: Default => STUBBED").append("\n");
+            help.append("\tParserVersion: Default => STUBBED").append("\n");
+
+            return help.toString();
+        }
+
+        //region Getters
+
+        /**
+         * Getter for assessmentFramework
+         *
+         * <p>getAssessmentFramework()</p>
+         *
+         * @return {@link String} - The assessmentFramework.
+         */
+        public String getAssessmentFramework() {
+            return assessmentFramework;
+        }
+
+        /**
+         * Getter for assessmentFrameworkVersion
+         *
+         * <p>getAssessmentFrameworkVersion()</p>
+         *
+         * @return {@link String} - The assessmentFrameworkVersion.
+         */
+        public String getAssessmentFrameworkVersion() {
+            return assessmentFrameworkVersion;
+        }
+
+        /**
+         * Getter for buildRootDir
+         *
+         * <p>getBuildRootDir()</p>
+         *
+         * @return {@link String} - The buildRootDir.
+         */
+        public String getBuildRootDir() {
+            return buildRootDir;
+        }
+
+        /**
+         * Getter for packageRootDir
+         *
+         * <p>getPackageRootDir()</p>
+         *
+         * @return {@link String} - The packageRootDir.
+         */
+        public String getPackageRootDir() {
+            return packageRootDir;
+        }
+
+        /**
+         * Getter for parserName
+         *
+         * <p>getParserName()</p>
+         *
+         * @return {@link String} - The parserName.
+         */
+        public String getParserName() {
+            return parserName;
+        }
+
+        /**
+         * Getter for parserVersion
+         *
+         * <p>getParserVersion()</p>
+         *
+         * @return {@link String} - The parserVersion.
+         */
+        public String getParserVersion() {
+            return parserVersion;
+        }
+        //endregion
+    }
+    //endregion
+}
