235a5294dfa1db63d4beff9599e41db5b42a8bbf
franceme <frantzme@vt.edu>
2019-03-31 22:23:52 -0400

    Upped Patch -> 03.03.04.
    Created samples folder for de-modularization of raw project structure.
    Entailing creating a single gradle project.
    
    Signed-off-by: franceme <frantzme@vt.edu>

---
+++ samples/testable-jar/src/main/java/tester/LiveVarsClass.java
@@ -0,0 +1,5 @@
+package tester;
+
+public class LiveVarsClass {
+    private static String xxx1 = VeryBusyClass.xxx;
+}
---
+++ samples/testable-jar/src/main/java/tester/VeryBusyClass.java
@@ -0,0 +1,12 @@
+package tester;
+
+import java.io.IOException;
+
+public class VeryBusyClass
+{
+	public static final String xxx = "aaaaaaa";
+
+	public void main(String[] args) throws IOException {
+		PasswordUtils.decryptPassword("helloworld");
+	}
+}
